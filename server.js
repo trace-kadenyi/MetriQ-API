5876fac80b7f4776883b566037f30db2b4029e35
Tracey Kadenyi <treykadenyi@gmail.com>
2025-06-05 12:00:10 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const port = process.env.PORT || 4000;
+
+// connect to MongoDB
+mongoose.connect(process.env.DATABASE_URI);
+
+// middleware to handle urlencoded data
+app.use(express.urlencoded({ extended: false }));
+
+// cors
+app.use(cors());
+
+// body-parser
+app.use(bodyparser.json());
+
+// middleware to handle json data
+app.use(express.json());
+
+// middleware to handle static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// start server
+mongoose.connection.once("open", () => {
+  console.log("connected to MongoDB");
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+  });
+});
