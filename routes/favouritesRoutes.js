7c920dca69c7d5c8f2196f69aa9469e0191084c5
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-06 17:36:07 +0300

    remove/add route comments

--- routes/favouritesRoutes.js
+++ routes/favouritesRoutes.js
@@ -1,12 +1,14 @@
-// routes/favourite.routes.js
 const express = require("express");
 const router = express.Router();
+
 const {
   getFavourites,
   toggleFavourite,
 } = require("../controllers/favouriteController");
 
+// get route
 router.get("/", getFavourites);
+// post route
 router.post("/toggle", toggleFavourite);
 
 module.exports = router;
