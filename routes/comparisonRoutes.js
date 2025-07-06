264e2eddc1190b7194513de22290f0ed5da1de4c
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-06 17:35:23 +0300

    Add comment for post route

--- routes/comparisonRoutes.js
+++ routes/comparisonRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { createComparison } = require("../controllers/comparisonController");
 
+// post route
 router.post("/compare", createComparison);
 
 module.exports = router;
